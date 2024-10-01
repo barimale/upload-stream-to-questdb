cb6dced773a4eef08d47d9279814dfff0ca1cd99
barimale <mateusz.wolnica@gmail.com>
2024-10-01 15:28:42 +0200

    Update FileModel.cs

--- UploadStreamToQuestDB.Application/Model/FileModel.cs
+++ UploadStreamToQuestDB.Application/Model/FileModel.cs
@@ -6,7 +6,7 @@ namespace File.Api.Controllers {
         public class FileModel {
             public IFormFile file;
             public string FilePath;
-            public List<FileModelState> State = new List<FileModelState>();
+            public ConcurrentBag<FileModelState> State = new List<FileModelState>();
        public string GetState()
        {
            return string.Join(",", this.State.Select(p => p.ToString()));
