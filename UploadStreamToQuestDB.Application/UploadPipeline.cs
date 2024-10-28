44144cf009d64c7f1bb291c083d3e8b42bc46f7d
Mateusz <mateusz.wolnica@gmail.com>
2024-10-28 20:12:32 +0100

    .

--- UploadStreamToQuestDB.Application/UploadPipeline.cs
+++ UploadStreamToQuestDB.Application/UploadPipeline.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using UploadStreamToQuestDB.Application.Handlers;
 using UploadStreamToQuestDB.Domain;
 
@@ -9,31 +10,38 @@ namespace UploadStreamToQuestDB.Application {
         private readonly IAntivirusHandler antivirusHandler;
         private readonly IDataIngestionerHandler dataIngestionerHandler;
         private readonly IDiskCleanUpHandler diskCleanUpHandler;
+        private readonly ILogger<UploadPipeline> _logger;
 
         public UploadPipeline(IUploadHandler uploadHandler,
             IExtensionHandler extensionHandler,
             IAntivirusHandler antivirusHandler,
             IDataIngestionerHandler dataIngestionerHandler,
-            IDiskCleanUpHandler diskCleanUpHandler) {
+            IDiskCleanUpHandler diskCleanUpHandler,
+            ILogger<UploadPipeline> logger) {
             this.uploadHandler = uploadHandler;
             this.extensionHandler = extensionHandler;
             this.antivirusHandler = antivirusHandler;
             this.dataIngestionerHandler = dataIngestionerHandler;
             this.diskCleanUpHandler = diskCleanUpHandler;
+            this._logger = logger;
         }
 
         public void Initialize(Controller controller) {
             uploadHandler.SetUploadHandler(controller);
-
+            _logger.LogInformation("Controller for UploadHandler is set.");
+            _logger.LogInformation("Start configure pipeline.");
             uploadHandler
                .HandleNext(extensionHandler)
                .HandleNext(antivirusHandler)
                .HandleNext(dataIngestionerHandler)
                .HandleNext(diskCleanUpHandler);
+            _logger.LogInformation("Pipeline configuration is ended.");
         }
 
         public async Task Run(FileModelsInput files) {
+            _logger.LogInformation("Start handling.");
             await uploadHandler.Handle(files);
+            _logger.LogInformation("Handling is ended.");
         }
     }
 }
