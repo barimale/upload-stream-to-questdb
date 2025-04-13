c37de8f3560d7715a500b66228ea20658c56a074
Mateusz <mateusz.wolnica@gmail.com>
2025-04-13 16:03:03 +0200

    .

--- UploadStreamToQuestDB.Application/IUploadPipeline.cs
+++ UploadStreamToQuestDB.Application/IUploadPipeline.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using UploadStreamToQuestDB.Domain;
 
 namespace UploadStreamToQuestDB.Application {
+    /// <summary>
+    /// Defines the interface for an upload pipeline.
+    /// </summary>
     public interface IUploadPipeline {
+        /// <summary>
+        /// Initializes the upload pipeline with the specified controller.
+        /// </summary>
+        /// <param name="controller">The controller to set for the upload handler.</param>
         void Initialize(Controller controller);
+
+        /// <summary>
+        /// Runs the upload pipeline with the specified files.
+        /// </summary>
+        /// <param name="files">The files to process.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
         Task Run(FileModelsInput files);
     }
 }
